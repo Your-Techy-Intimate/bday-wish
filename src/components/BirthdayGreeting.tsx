72ad2b7073d47ffb8fe433d99c6fa629d8994c22
aditya123r-u <154539010+aditya123r-u@users.noreply.github.com>
2025-01-22 19:28:38 +0530

    Saying byee

--- src/components/BirthdayGreeting.tsx
+++ src/components/BirthdayGreeting.tsx
@@ -4,7 +4,7 @@ import { useNavigate } from 'react-router-dom';
 import { Heart, Sparkles } from 'lucide-react';
 
 const messages = [
-  "Hello Madam Jiiii",
+  "Byee Madam Jiiii",
   "It's Your Special Day Yeyey!",
   "So, i had to make something special for you cause you are special to me!",
   "Do you wanna see what I made??"
